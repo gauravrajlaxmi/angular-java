9eba1b9ad0167fd41b090c4cdb905c94b1c26c39
Gaurav Karemore <95206483+gauravrajlaxmi@users.noreply.github.com>
2025-12-23 09:39:38 +0530

    Change getUrl to relative API endpoint
    Updated the getUrl to use a relative path instead of an absolute URL.

--- angular-frontend/src/app/services/worker.service.ts
+++ angular-frontend/src/app/services/worker.service.ts
@@ -9,7 +9,9 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://54.83.90.121:8080/api/v1/workers";
+  // private getUrl: string = "http://54.83.90.121:8080/api/v1/workers";
+  private getUrl = "/api/v1/workers";
+
 
   constructor(private _httpClient: HttpClient) { }
 
